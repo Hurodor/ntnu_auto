11268392b74800a80da3f8a7d6d5262ee32dbc06
Hurodor <vegkar@stud.ntnu.no>
2021-09-08 14:09:31 +0200

    project finished
    - only bugfixes left

--- commands.py
+++ commands.py
@@ -3,6 +3,7 @@ import config
 import json
 
 from ntnu import NTNU
+import power
 from supported_settings import default_room_settings as df_sett
 from supported_settings import valid_room_settings as valid_sett
 
@@ -36,7 +37,7 @@ def main():
 
 # Prints out current settings
 @click.command()
-def g_settings():
+def get_settings():
     current_settings = config.get_settings()
     printstr = ""
     for key, value in current_settings['room_settings'].items():
@@ -84,12 +85,54 @@ def book():
     ntnu.book_room(**settings)
 
 
+@click.command()
+@click.option('--time', '-t', prompt="You have to choose time: ")
+def setup_auto(time):
+    if power.check_if_task_exsists():
+
+        # prompt for choosing days
+        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        print("Spesify with days you want to auto book on (y/n) (default = yes): \n")
+        for day in days:
+            decision = take_input(day + ": ")
+            if not decision or decision == "y":
+                continue
+            days.remove(day)
+
+        # this updates the schedualed task
+        power.change_trigger(days=days, time=time)
+        return
+
+    # Create task if there is no allready exsisting
+    power.make_task(time)
+
+
+@click.command()
+def auto_delete():
+    desicison = input("Are you sure? (y/n) defalut=y")
+    if desicison == "n":
+        return
+
+    power.delete_task()
+
+
+@click.command()
+@click.option("--enable/--disable", '-e/-d')
+def toogle_active(enable):
+    if enable:
+        power.toggle_activation(False)
+
+    power.toggle_activation()
+
 
 main.add_command(login)
-main.add_command(g_settings)
+main.add_command(get_settings)
 main.add_command(set_room)
 main.add_command(book)
 main.add_command(supported_rooms)
+main.add_command(setup_auto)
+main.add_command(auto_delete)
+main.add_command(toogle_active)
 
 if __name__ == '__main__':
     main()
---
+++ power.py
@@ -0,0 +1,64 @@
+import subprocess
+import config
+
+
+def run(cmd, output=True):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+
+    if output:
+        if completed.returncode != 0:
+            print("An error occured: {}".format(completed.stderr))
+
+        print(completed.stdout.decode())
+
+    return completed
+
+
+def make_task(time):
+    command = """
+$action = New-ScheduledTaskAction -Execute "{}\\venv\\Scripts\\python.exe" -Argument "{}\\main.py"
+$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek @("Monday", "Tuesday", "Wednesday", "Thursday", "Friday") -At {}
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -RunOnlyIfNetworkAvailable
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "ntnu_auto_booking" -Description "This will enable automaticly room booking at ntnu" -Settings $Settings
+""".format(config.path_to_project_folder,
+           config.path_to_project_folder,
+           time)
+
+    run(command)
+
+
+def toggle_activation(disable=True):
+    command = """{}-ScheduledTask -TaskName "ntnu_auto_booking" """.format("Disable" if disable else "Enable")
+
+    run(command)
+
+
+def delete_task():
+    command = """Unregister-ScheduledTask -TaskName "ntnu_auto_booking" -Confirm:$false"""
+
+    run(command)
+
+
+def change_trigger(days: list, time):
+    """takes in list of days to book on, and time and change task scheduler"""
+
+    days_format = '''"{}", ''' * len(days)
+    days_str = "@(" + days_format[:-2] + ")"
+    day_str_finished = days_str.format(*days)
+
+    command = """
+$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek {} -At {}
+Set-ScheduledTask -TaskName ntnu_auto_booking" -Trigger $Trigger""
+""".format(day_str_finished, time)
+
+    run(command)
+
+
+def check_if_task_exsists():
+    command = """Get-ScheduledTask -TaskName "ntnu_auto_booking" """
+    response = run(command, False)
+
+    if response.returncode == 0:
+        return True
+    return False
+
